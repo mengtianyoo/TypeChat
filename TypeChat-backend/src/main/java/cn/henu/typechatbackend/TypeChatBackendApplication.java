e773d42ac7f54ebe6e7fca54343b034b7f9d2258
3126058535 <3126058535@qq.com>
2024-03-07 01:06:22 +0800

    提交空白Typechat后端

---
+++ TypeChat-backend/src/main/java/cn/henu/typechatbackend/TypeChatBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.henu.typechatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeChatBackendApplication.class, args);
+    }
+
+}
---
+++ TypeChat-backend/src/test/java/cn/henu/typechatbackend/TypeChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.henu.typechatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
